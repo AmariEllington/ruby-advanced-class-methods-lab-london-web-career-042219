99210d8aa41b274256aa1eb756bb14d2f20e0903
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-28 19:57:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,11 +36,11 @@ class Song
 
   def self.find_or_create_by_name(name)
 
-    if self.find_by_name(name) == nil
-        self.create_by_name(name)
-      else
-        self.find_by_name(name)
-      end
+    #if self.find_by_name(name) == nil
+    #    self.create_by_name(name)
+    #  else
+    #    self.find_by_name(name)
+    #  end
     self.find_by_name || self.create_by_name
   end
 
