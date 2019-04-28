ec3021dbc335394b9ef3e8b3a0397adb2c0aeb53
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-28 20:04:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
     self.find_by_name(name) || self.create_by_name(name)
   end
 
-  def self.alphabetical
+  def self.alphabetical(name)
   alphabetical_names =  @@all.each.name do |x| x.name
     alphabetical_names.sort
   end
